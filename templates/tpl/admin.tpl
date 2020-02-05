fd2dc0bc8784be3df001f56b718789843307db4b
lichiu <a0965155288@gmail.com>
2020-02-05 14:13:19 +0800

    v1.0

---
+++ templates/tpl/admin.tpl
@@ -0,0 +1,20 @@
+<h1 class="text-center">育將電腦工作室 後台</h1>
+<div class="container">
+	<div class="row">
+		<div class="col-sm-9">
+
+		</div>
+		<div class="col-sm-3">
+			<div class="card" style="width: 18rem;">
+				<div class="card-header">
+				  Featured
+				</div>
+				<ul class="list-group list-group-flush">
+				  <li class="list-group-item">Cras justo odio</li>
+				  <li class="list-group-item">Dapibus ac facilisis in</li>
+				  <li class="list-group-item">Vestibulum at eros</li>
+				</ul>
+			  </div>
+		</div>
+	</div>
+</div>
---
+++ templates_c/35a4d301a82e62d603a6a8357cdcffdab2186244_0.file.admin.tpl.php
@@ -0,0 +1,44 @@
+<?php
+/* Smarty version 3.1.34-dev-7, created on 2020-02-05 07:08:46
+  from 'C:\xampp\htdocs\web11\templates\tpl\admin.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.34-dev-7',
+  'unifunc' => 'content_5e3a5bee482c52_59530357',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '35a4d301a82e62d603a6a8357cdcffdab2186244' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\web11\\templates\\tpl\\admin.tpl',
+      1 => 1580882729,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5e3a5bee482c52_59530357 (Smarty_Internal_Template $_smarty_tpl) {
+?><h1 class="text-center">育將電腦工作室 後台</h1>
+<div class="container">
+	<div class="row">
+		<div class="col-sm-9">
+
+		</div>
+		<div class="col-sm-3">
+			<div class="card" style="width: 18rem;">
+				<div class="card-header">
+				  Featured
+				</div>
+				<ul class="list-group list-group-flush">
+				  <li class="list-group-item">Cras justo odio</li>
+				  <li class="list-group-item">Dapibus ac facilisis in</li>
+				  <li class="list-group-item">Vestibulum at eros</li>
+				</ul>
+			  </div>
+		</div>
+	</div>
+</div><?php }
+}
